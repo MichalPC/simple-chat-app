16e831aea806a433cfbd6af101a0f4199998463a
MichalPC <zfac211@rhul.ac.uk>
2020-11-03 18:34:18 +0000

    Add an event listener for message submission.

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const socket = io('http://localhost:3000')
+const msgForm = document.getElementById('msg-input-container')
+const msgInput = document.getElementById('msg-input')
 
 socket.on('chat-message', data =>{
     console.log(data)
+})
+
+msgForm.addEventListener('submit', e=>{
+    e.preventDefault()
+    const message = msgInput.value
+    socket.emit('send-chat-message', message)
+    msgInput.value=""
 })
