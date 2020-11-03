91f8c5e18ea95e17d923b41b1bdde878e26332b6
MichalPC <zfac211@rhul.ac.uk>
2020-11-03 18:41:39 +0000

    Make the server broadcast messages to other clients.

--- server.js
+++ server.js
@@ -3,4 +3,9 @@ const io = require('socket.io')(3000)
 io.on('connection', socket => {
     socket.emit('chat-message',
                 'Welcome to the simple-chat-app!')
+
+    socket.on("send-chat-message", message => {
+        socket.broadcast.emit('chat-message',
+                              message)
+    })
 })
